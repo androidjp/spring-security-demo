0ed6fd9de025db07eddb94e172ee18dc7d3be0a5
Jasper Wu <jasper.wu@cargosmart.com>
2020-06-02 13:31:55 +0800

    自动登录三：二次校验

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -44,6 +44,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/remember").rememberMe()
+                .antMatchers("/admin").fullyAuthenticated()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -10,8 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    // 第一个 /hello 接口，只要认证后就可以访问，无论是通过用户名密码认证还是通过自动登录认证，只要认证了，就可以访问。
     @GetMapping("hello")
     public String hello() {
         return "hello";
+    }
+
+    // 第二个 /admin 接口，必须要用户名密码认证之后才能访问，如果用户是通过自动登录认证的，则必须重新输入用户名密码才能访问该接口。
+    @GetMapping("admin")
+    public String admin() {
+        return "admin";
+    }
+
+    // 第三个 /rememberme 接口，必须是通过自动登录认证后才能访问，如果用户是通过用户名/密码认证的，则无法访问该接口。
+    @GetMapping("remember")
+    public String remember() {
+        return "remember";
     }
 }
